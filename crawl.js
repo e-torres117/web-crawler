d647045c1f4dd4e09948c63e94e921e438134448
e-torres117 <torres.eduardo117@gmail.com>
2024-01-05 17:32:50 -0800

    main crawl funciton

--- crawl.js
+++ crawl.js
@@ -1,7 +1,25 @@
 const {JSDOM} = require('jsdom')
 
 
-async function crawlPage(currentURL){
+async function crawlPage(baseURL, currentURL, pages){
+
+
+    const baseURLObj = new URL(baseURL)
+    const currentURLObj = new URL(currentURL)
+    if( baseURLObj.hostname !== currentURLObj.hostname) {
+        return pages
+    }
+
+    const normalizeCurrentURL = normalizeURL(currentURL)
+
+    if ( pages[normalizeCurrentURL] > 0 ){
+        pages[normalizeCurrentURL]++
+        return pages
+    }
+
+
+    pages[normalizeCurrentURL] = 1
+
     console.log(`actively crawling: ${currentURL}`)
 
     try {
@@ -9,20 +27,26 @@ async function crawlPage(currentURL){
 
         if(resp.status > 399){
             console.log(`error in fetch with status code: ${resp.status} on page ${currentURL}`)
-            return 
+            return pages
         }
 
         const contentType = resp.headers.get("content-type")
         if(!contentType.includes("text/html")){
             console.log(`non html resp, content type: ${contentType}, on page:${currentURL}`)
-            return 
+            return pages
         }
 
-        console.log(await resp.text())
+        const htmlBody = await resp.text()
+        const nextUrls = getURLsFromHTML(htmlBody, baseURL)
+
+        for (const nextUrl of nextUrls){
+            pages = await crawlPage(baseURL, nextUrl, pages)
+        }
+
     } catch (error) {
         console.log(`error in fetch: ${error.message}, on page ${currentURL}`)
     }
-
+    return pages
 }
 
 function getURLsFromHTML(htmlBody, baseUrl){
